85d66344efa7efb5f1a2eca588184dfba6825f6a
Andrew McLeod <admcleod@gmail.com>
2016-10-05 19:18:05 +0200

    lots of changes

--- lib/charms/layer/woodpecker_tools.py
+++ lib/charms/layer/woodpecker_tools.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 
-import os
+import shlex
 import subprocess
-import re
 from charmhelpers.core import hookenv
 
 
@@ -14,15 +13,18 @@ def safe_status(workload, status):
 
 def woodpecker_listen():
     cfg = hookenv.config()
-    ports = cfg.get('check_ports')
+    ports = cfg.get('check_ports').split(',')
     for port in ports:
+        hookenv.log('Opening local port: {}'.format(port))
         open_local_port(port)
 
 
 def open_local_port(port):
     try:
-        os.spawnl(os.P_NOWAIT, '/bin/nc -k -l {}'.format(port))
-    except os.error as msg:
+        exec_string = "/bin/nc -k -l {}".format(port)
+        exec_string = shlex.split(exec_string)
+        subprocess.Popen(exec_string, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as msg:
         hookenv.log('something went wrong with netcat listening: {}'.format(msg))
 
 
@@ -36,49 +38,67 @@ def _nc_method(send_string):
     return result, stderr
 
 
+
 def check_port(label, host, port, send='', receive=''):
     if receive != '':
         send_string = 'echo {} | nc {} {}'.format(send, host, port)
+        hookenv.log('Port check, label: {}, host: {}, port: {}, send: {}, receive: {}'.format(label, host, port, send, receive))
         result = _nc_method(send_string) + receive
     else:
         send_string = 'nc {} {}'.format(host, port)
-        result = _nc_method(send_string) 
+        hookenv.log('Port check, label: {}, host: {}, port: {}'.format(label, host, port))
+        result = _nc_method(send_string)
     hookenv.log('nc command: {}, result: {}'.format(send_string, result), 'DEBUG')
     return result
 
 
 def check_peers(peers):
     cfg = hookenv.config()
-    ports = cfg.get('check_ports')
+    ports = cfg.get('check_ports').split(',')
     try:
         peer_fail
     except NameError:
         peer_fail = []
     for peer in peers:
         for port in ports:
-            hookenv.log('Checking Peer: {}, Port: {}'.format(peer[1], port), 'INFO')
+            hookenv.log('Checking Peer: {}, {}, Port: {}'.format(peer[0], peer[1], port), 'INFO')
             peer_check = check_port('peer', peer[1], port)
-    if peer_check[1] == 1:
-        peer_fail.append(peer[1])
-    if peer_fail != []:
-        safe_status('peers failed: ' + str(peer_fail))
-    else:
-        safe_status('peers ok')
+        if peer_check[1] == 1:
+            if peer[0].split('/')[1] not in peer_fail:
+                peer_fail.append(peer[0].split('/')[1])
+        else:
+            if peer[0].split('/')[1] in peer_fail:
+                peer_fail.remove(peer[0].split('/')[1])
+    return peer_fail
 
 
 def check_remote_hosts():
+    try:
+        hosts_failed
+    except NameError:
+        hosts_failed = []
     cfg = hookenv.config()
-    remote_checks = cfg.get('check_list')
+    remote_checks = cfg.get('check_list').split(',')
+    hookenv.log('REMOTE CHECKS: {}'.format(remote_checks))
     if remote_checks != '':
         for check in remote_checks:
-            result = check_port(check.split(':')).strip('[]')
-            if len(check.split(':')) == 3:
+            check_list = check.split(':')
+            result = check_port(check_list[0], check_list[1], check_list[2])
+            hookenv.log('RESULT: {}'.format(result))
+            if len(check_list) == 3:
                 # Send is not defined
-                if check.split(':')[3] in result:
-                    return 'OK'
+                if result[1] == 0:
+                    if check_list[0] in hosts_failed:
+                        hosts_failed.remove(check_list[0])
                 else:
-                    return 'FAILED'
-            if check.split(':')[4] in result:
-                return 'OK'
-            else:
-                return 'FAILED'
+                    if check_list[0] not in hosts_failed:
+                        hosts_failed.append(check_list[0])
+            elif len(check_list) > 3:
+                # this definitely wont work yet...
+                if check.split(':')[3] in result:
+                    if check.split(':')[0] in hosts_failed:
+                        hosts_failed.remove(check.split(':')[0])
+                else:
+                    if check.split(':')[0] not in hosts_failed:
+                        hosts_failed.append(check.split(':')[0])
+    return hosts_failed
--- reactive/woodpecker.py
+++ reactive/woodpecker.py
@@ -1,6 +1,5 @@
 # pylint: disable=unused-argument
 from charms.reactive import when, when_not, set_state, remove_state
-from charms.reactive.bus import get_states
 from charmhelpers.core import hookenv
 from charms.layer.woodpecker_tools import check_peers, check_remote_hosts, safe_status, woodpecker_listen
 
@@ -12,15 +11,37 @@ if check_hosts:
 else:
         remove_state('check_hosts')
 
-def _set_states(check_result):
-    if 'fail' in check_result['icmp']:
+
+def _set_states(peers=None, hosts=None):
+    # if peers calls this, hosts is none
+    # if hosts calls its, peers is none
+    # clearly i need a way to store values somewhere outside of the function
+    # i will call a 'check all' function which will do so
+    # it will just not rely on reactive states
+
+    hookenv.log('_set_states PEERS: {}'.format(peers))
+    hookenv.log('_set_states HOSTS: {}'.format(hosts))
+    if peers is None:
+        peer_status = 'waiting for peers'
+    elif peers != []:
         set_state('woodpecker-peer.failed')
+        peer_status = 'peer check failed: ' + str(peers)
     else:
-        remove_state('woodpecker-icmp.failed')
-    if 'fail' in check_result['dns']:
-        set_state('woodpecker-dns.failed')
+        remove_state('woodpecker-peer.failed')
+        peer_status = 'peer check ok'
+    if hosts is None:
+        hostcheck_status = ', no host checks defined'
+    elif hosts != []:
+        set_state('woodpecker-hosts.failed')
+        hostcheck_status = ', host check failed: ' + str(hosts)
     else:
-        remove_state('woodpecker-dns.failed')
+        remove_state('woodpecker-hosts.failed')
+        hostcheck_status = ', host check ok'
+    if 'failed' in peer_status or 'failed' in hostcheck_status:
+        workload = 'blocked'
+    else:
+        workload = 'active'
+    safe_status(workload, peer_status + hostcheck_status)
 
 
 @when_not('woodpecker.listening')
@@ -30,10 +51,10 @@ def open_woodpecker_port():
 
 @when_not('woodpecker.joined', 'check_hosts')
 def no_peers():
-    safe_status('waiting', 'No TCP checks configured, Waiting for peers...')
+    safe_status('waiting', 'waiting for peers, no host checks defined')
 
 
-@when('woodpecker.joined')
+@when('woodpecker.joined', 'woodpecker.listening')
 def check_peers_joined(woodpecker):
     '''
     We do not dismiss joined here so that this check reruns
@@ -41,17 +62,17 @@ def check_peers_joined(woodpecker):
     '''
 
     nodes = woodpecker.get_nodes()
-    _set_states(check_peers(nodes))
+    _set_states(check_peers(peers=nodes))
 
 @when('check_hosts')
-def check_hosts(woodpecker):
+def check_remote_hosts_labels():
     check_results = check_remote_hosts()
-    active_states = get_states()
-    for result in check_results:
-        result_string = result.split(":")
-        result_label = result_string[0]
-        for state in active_states:
-            if state not in check_results:
-                remove_state(state)
-        set_state(result)
+    _set_states(check_hosts(hosts=check_results))
+    #for result in check_results:
+    #    result_string = result.split(":")
+    #    result_label = result_string[0]
+    #    for state in active_states:
+    #        if state not in check_results:
+    #            remove_state(state)
+    #    set_state(result)
 
